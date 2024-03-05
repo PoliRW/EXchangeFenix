0a59aaa284cfae4c6865c4dfa2162d52e884bcfe
Polina Rwchdi <halilis@seznam.cz>
2024-03-05 14:48:07 +0100

    Files in which the registration of admins is being handled

---
+++ db/uzivatel.sql
@@ -0,0 +1,5 @@
+CREATE TABLE uzivatel (
+    id int unsigned primary key AUTO_INCREMENT,
+     jmeno VARCHAR(100),
+      heslo VARCHAR(200)
+)
---
+++ uzivatel.php
@@ -0,0 +1,41 @@
+<?php
+
+$db = new PDO(
+    "mysql:host = localhostl;dbname=exchange;charset=utf8",
+    "root",
+    "",
+    array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ),
+);
+
+class Uzivatel
+{
+    public $id;
+    public $jmeno;
+    public $heslo;
+
+
+    function __construct($jmenoArg, $hesloArg)
+    {
+        $this->jmeno = $jmenoArg;
+        $this->heslo = $hesloArg;
+    }
+    function registrovat()
+    {
+        global $db;
+
+        // Kontrola počtu registrovaných adminů
+        $dotazAdmini = $db->query("SELECT COUNT(*) FROM uzivatel WHERE role = 'admin'");
+        $pocetAdminu = $dotazAdmini->fetchColumn();
+
+        if ($pocetAdminu < 2) {
+            $dotaz = $db->prepare("INSERT INTO uzivatel SET jmeno=? , heslo = ? , role = 'admin'");
+            $dotaz->execute([$this->jmeno, $this->heslo]);
+            echo "Admin byl úspěšně registrován.";
+        } else {
+            echo "Byl dosažen maximální počet adminů.";
+        }
+    }
+}
+
