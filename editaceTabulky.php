dad83b8f467f279fc6874d5b6d585ed7c4e8b8f5
Polina Rwchdi <halilis@seznam.cz>
2024-03-11 14:17:18 +0100

    Processing the function:` ulozitCenaNakup`

--- editaceTabulky.php
+++ editaceTabulky.php
@@ -1,44 +1,31 @@
 <?php
 session_start();
-require_once 'listek.php';
+require "listek.php";
 // vyreset otvirane stranku s editaceTabula otvira se i bez prihlaseni
 
 //  zpracovani vyberu aktualni radek
 $idRadek = "";
 $instanceAktualniRadek = "";
+$novaCenaNakup = "";
 if (array_key_exists("radek", $_GET)) {
     $idRadek = $_GET["radek"];
     $instanceAktualniRadek = $seznamListek[$idRadek];
 }
-
-// zpracovani formulare pro ulozeni cena prodej nebo a cena nakup
-//pro kazda instace - radek se nacete vlastni cena nakup a cena prodej
-//$obsah bereme z admin formulare pro ulozeni
-
-if (array_key_exists("ulozit", $_POST)) {
-    // Pokuste se přečíst vlastnost "menaKod" na řetězci 
-    $instanceAktualniRadek->menaKod;
-    $instanceAktualniRadek->cenaNakup;
-    $instanceAktualniRadek->ulozitCena();
-    var_dump($instanceAktualniRadek->ulozitCena());
+// zpracováním dat /ulozit nova data ziskani s tabulki
+// Tento kód hledá a ukládá novou cenu nákupu pro vybranou měnu po stisknutí tlačítka "uložit" ve formuláři. Pole "menaKod" a "cenaNakup" v HTML formuláři jsou nastavena jako pole v atributu "name", což umožňuje snadnější získání indexu těchto polí v PHP skriptu pro další zpracování.
+if (array_key_exists('ulozit', $_POST)) {
+    $indexMenaKod = array_search($idRadek, $_POST['menaKod']);
+    $poleCenaNakup = ($_POST['cenaNakup']);
+    foreach ($poleCenaNakup as $indexCenaNakup => $cenaNakup) {
+        // Pokud se index měny shoduje s indexem ceny nákupu, uloží se nová cena nákupu do proměnné $novaCenaNakup.
+        if ($indexMenaKod === $indexCenaNakup) {
+            $novaCenaNakup = $cenaNakup;
+            if($novaCenaNakup!=0){
+             $instanceAktualniRadek->ulozitCenaNakup($novaCenaNakup,$idRadek);
+            }
+        }
+    }
 }
-// $instanceAktualniRadek->cenaNakup
-// $puvodniCenaProdej = $instanceAktualniRadek->cenaProdej;
-// $instanceAktualniRadek->tcenaProdej = $_GET['cenaProdej'];
-// echo $puvodniCenaNakup ;
-
-
-
-
-//         $obsah = $_POST["obsah"];
-//         $instanceAktualniStranky->setObsah($obsah);
-//         // presmerujeme se na url s editaci stranky s novym id
-//         // (kdyby se id zmenilo tak nesmime zustat na puvodni url)
-//         //urlencode prepise url  kvuli neplatni znaci v url
-//         header("Location: ?stranka=" . urlencode($instanceAktualniStranky->id));
-//         //poznamenat provedena zmena
-//     }
-
 ?>
 
 <!DOCTYPE html>
@@ -48,6 +35,7 @@ if (array_key_exists("ulozit", $_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css/styleAdmin.css">
+    <link rel="icon" type="image/png" href="image/admin.png">
     <title>Document</title>
 
 </head>
@@ -56,15 +44,11 @@ if (array_key_exists("ulozit", $_POST)) {
 
     <main class='padding'>
         <form method="post" class="styleBox">
-            <!-- <label id="nazevMena"> Název měny </label> -->
-
-            <!-- <label id=""> Kód </label>
-        <label id=""> Nas.</label><label id=""> Nákup/</label><label id=""> Prodej/Sell</label> -->
 
             <?php if (array_key_exists("prihlasenyUzivatel", $_SESSION) == false) {
 
                 // Zpracování chyb
-                echo " Chybně zadané heslo nebo uživatelské jméno" . "<br/>" ."<br/>".
+                echo " Chybně zadané heslo nebo uživatelské jméno" . "<br/>" . "<br/>" .
                     "<a href='http://localhost/EXchangeFenix/admin.php'>➡️ Návrat zpět do přihlašovacího formuláře</a>";
             }
             // sekce pro prihlaseni uživatele 
@@ -81,24 +65,25 @@ if (array_key_exists("ulozit", $_POST)) {
                     </tr>
                     <?php
                     if (!(array_key_exists("ulozit", $_POST))) {
+                        //Vypsaný lístek z databáze
                         foreach ($seznamListek as $klicRadek => $instanceRadek) { ?>
                             <tr>
                                 <td>
                                     <a <?php
                                         echo "href='?radek=$instanceRadek->menaKod'" ?>><?php echo $instanceRadek->menaNazev; ?></a>
 
-                                    <!-- link nefunguje pri method post nachradim button
-                            <button name="radek" <php echo "value= '$instanceRadek->menaKod'" ?>><php echo $instanceRadek->menaNazev; ?></button>  -->
-
                                 </td>
                                 <td>
-                                    <input type="text" name="menaKod" value="<?php echo htmlspecialchars($instanceRadek->menaKod); ?>" />
+                                    <!--  pomoci "menaKod[]" nastavuje pole  ve formuláři -->
+                                    <form>
+                                        <input type="text" name="menaKod[]" value="<?php echo htmlspecialchars($instanceRadek->menaKod); ?>" />
                                 </td>
                                 <td>
                                     <input type="text" name="mnozstvi" value="<?php echo htmlspecialchars($instanceRadek->mnozstvi); ?>" />
                                 </td>
                                 <td>
-                                    <input type="text" name="cenaNakup" required value="<?php echo htmlspecialchars($instanceRadek->cenaNakup); ?>" />
+                                    <!--  pomoci "cenaNakup[]" nastavuje pole  ve formuláři -->
+                                    <input type="text" name="cenaNakup[]" required value="<?php echo htmlspecialchars($instanceRadek->cenaNakup); ?>" />
                                 </td>
                                 <td>
                                     <input type="text" name="cenaProdej" required value="<?php echo htmlspecialchars($instanceRadek->cenaProdej); ?>" />
@@ -108,15 +93,12 @@ if (array_key_exists("ulozit", $_POST)) {
                         <?php
                         }
                     } else {
+                        //Vypsaný lístek s aktualizovanými daty, která již byla uložena do databáze
                         foreach ($seznamListek as $klicRadek => $instanceRadek) { ?>
                             <tr>
                                 <td>
                                     <a <?php
                                         echo "href='?radek=$instanceRadek->menaKod'" ?>><?php echo $instanceRadek->menaNazev; ?></a>
-
-                                    <!-- link nefunguje pri method post nachradim button
-                            <button name="radek" <php echo "value= '$instanceRadek->menaKod'" ?>><php echo $instanceRadek->menaNazev; ?></button>  -->
-
                                 </td>
                                 <td>
                                     <input type="text" name="menaKod" value="<?php echo htmlspecialchars($instanceRadek->menaKod); ?>" />
@@ -131,16 +113,18 @@ if (array_key_exists("ulozit", $_POST)) {
                                     <input type="text" name="cenaProdej" required value="<?php echo htmlspecialchars($instanceRadek->cenaProdej); ?>" />
                                 </td>
                             </tr>
-                    <?Php    }
+                    <?php }
                     }
                     ?>
-
                 </table>
                 <button name='ulozit'>Uložit</button>
         </form>
         <a href='http://localhost/EXchangeFenix/admin.php'>edit</a>
     <?php    //"konec podmínka, která určuje, zda je uživatel přihlášen"
             } ?>
+
+
+
     </main>
 
 </body>
